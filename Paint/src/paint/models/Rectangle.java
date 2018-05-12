4fe3f66bdc103aedce664507d955d06fdce9ef76
MohamedTarek1995 <36802648+MohamedTarek1995@users.noreply.github.com>
2018-05-12 02:06:38 +0200

    rectangle try

---
+++ Paint/src/paint/models/Rectangle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paint.models;
+
+/**
+ *
+ * @author m1
+ */
+public class Rectangle extends Shape {
+    
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getCircumference() {
+        return 2*width+2*height;
+    }
+    
+}
